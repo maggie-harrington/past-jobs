f59ebfaa48c7410bbb12b37cce5cf4d885679ac1
Leah Sherrell and Maggie Harrington <maggie@>
2017-02-08 13:45:32 -0800

    add Job class with constructor, getters and setters

---
+++ src/Job.php
@@ -0,0 +1,46 @@
+<?php
+class Job
+{
+    private $company;
+    private $date_range;
+    private $position;
+
+    function __construct($company, $date_range, $position)
+    {
+        $this->company = $company;
+        $this->date_range = $date_range;
+        $this->position = $position;
+    }
+
+    function setCompany($new_company)
+    {
+        $this->company = (string) $new_company;
+    }
+
+    function setDateRange($new_date_range)
+    {
+        $this->date_range = (string) $new_date_range;
+    }
+
+    function setPosition($new_position)
+    {
+        $this->position = (string) $new_position;
+    }
+
+    function getCompany()
+    {
+        return $this->company;
+    }
+
+    function getDateRange()
+    {
+        return $this->date_range;
+    }
+
+    function getPosition()
+    {
+        return $this->position;
+    }
+
+}
+?>
